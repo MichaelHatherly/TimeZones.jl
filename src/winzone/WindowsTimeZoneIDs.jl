caa88fb6ff6571a342fd2dfd70878651c80dbe42
Mike <michaelhatherly@gmail.com>
2021-11-02 11:29:03 +0000

    Use `RelocatableFolders` for timezones build artifacts.

--- src/winzone/WindowsTimeZoneIDs.jl
+++ src/winzone/WindowsTimeZoneIDs.jl
@@ -1,6 +1,7 @@
 module WindowsTimeZoneIDs
 
 using ...TimeZones: DEPS_DIR
+using RelocatableFolders
 using Future: copy!
 
 if VERSION >= v"1.3"
@@ -16,8 +17,8 @@ const UNICODE_CLDR_VERSION = "release-39"
 const WINDOWS_ZONE_URL = "https://raw.githubusercontent.com/unicode-org/cldr/$UNICODE_CLDR_VERSION/common/supplemental/windowsZones.xml"
 const WINDOWS_ZONE_FILE = joinpath("cldr-$UNICODE_CLDR_VERSION", "common", "supplemental", "windowsZones.xml")
 
-const WINDOWS_XML_DIR = joinpath(DEPS_DIR, "local")
-const WINDOWS_XML_FILE = joinpath(WINDOWS_XML_DIR, "windowsZones.xml")
+const WINDOWS_XML_DIR = @path joinpath(DEPS_DIR, "local")
+const WINDOWS_XML_FILE = @path joinpath(WINDOWS_XML_DIR, "windowsZones.xml")
 
 const WINDOWS_TRANSLATION = Dict{String, String}()
 
